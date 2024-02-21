0b37354f0f344bc4f749946c52efc3c0b3656b09
JulioSimeon <jiggysimeon@gmail.com>
2024-02-21 18:27:34 +0800

    Define SpawnEnemies function and implement win/lose condition

--- Source/BulletHell/BulletHellGameMode.cpp
+++ Source/BulletHell/BulletHellGameMode.cpp
@@ -10,7 +10,7 @@
 
 void ABulletHellGameMode::ActorDied(AActor* DeadActor)
 {
-
+    bool bBossShipIsAlive = true;
     if(DeadActor == PlayerShip)
     {
         PlayerShip->HandleDestruction();
@@ -18,18 +18,28 @@ void ABulletHellGameMode::ActorDied(AActor* DeadActor)
         {
             BulletHellPlayerController->SetPlayerEnabledState(false);
             //PlayerShip->GetShipPlayerController()->bShowMouseCursor= false;
+            GameOver(false);
         }
     }
     else if(AEnemyShip* EnemyShip = Cast<AEnemyShip>(DeadActor))
     {
-        if(ABossShip* BossShip = Cast<ABossShip>(EnemyShip))//if EnemyShip if a BossShip
+        if(ABossShip* BossShip = Cast<ABossShip>(EnemyShip))//if EnemyShip is a BossShip
         {
             if(ABossShip* Parent = Cast<ABossShip>(BossShip->GetOwner()))//if BossShip has a parent and the parent is still alive
             {
                 Parent->UpdateChildShipStatus(BossShip);//Let parent know that its child has died so that it can spawn new child
             }
+            else//if main BossShip has died
+            {
+                bBossShipIsAlive = false;
+            }
         }
         EnemyShip->HandleDestruction();
+
+        if(GetAliveEnemyShipsCount() == 0 && !bBossShipIsAlive)//if there are no more EnemyShips and main BossShip is dead remaining player has won
+        {
+            GameOver(true);
+        }
     }
 }
 
@@ -47,15 +57,71 @@ void ABulletHellGameMode::HandleGameStart()
     StartGame();
 
     if(BulletHellPlayerController)
-        {
-            BulletHellPlayerController->SetPlayerEnabledState(false);
-            FTimerHandle PlayerEnableTimerHandle;
-            FTimerDelegate PlayerEnableTimerDelegate = FTimerDelegate::CreateUObject(BulletHellPlayerController, &ABulletHellPlayerController::SetPlayerEnabledState, true);
-            GetWorldTimerManager().SetTimer(PlayerEnableTimerHandle, PlayerEnableTimerDelegate, StartDelay, false);
-        }
+    {
+        BulletHellPlayerController->SetPlayerEnabledState(false);
+        FTimerHandle PlayerEnableTimerHandle;
+        FTimerDelegate PlayerEnableTimerDelegate = FTimerDelegate::CreateUObject(BulletHellPlayerController, &ABulletHellPlayerController::SetPlayerEnabledState, true);
+        GetWorldTimerManager().SetTimer(PlayerEnableTimerHandle, PlayerEnableTimerDelegate, StartDelay, false);
+    }
+
 }
 
 void ABulletHellGameMode::SpawnEnemies()
 {
+    if(EnemySpawnsDataTable)
+    {
+        //if row is valid/found
+        if(FEnemySpawns* EnemySpawn = EnemySpawnsDataTable->FindRow<FEnemySpawns>(FName(FString::FromInt(SpawnSequenceCounter)), "Enemy Spawn Context"))
+        {
+            UE_LOG(LogTemp, Display, TEXT("Row: %d"), SpawnSequenceCounter);
+            //while actual spawn < spawn count
+            for(int index = 0; index <= EnemySpawn->SpawnCount; index++)
+            {
+                //keep spawning
+                    //Delay before next spawn count (SpawnCountDelay)
+                    FTimerHandle SpawnDelayTimerHandle;
+                    FTimerDelegate SpawnDelayTimerDelegate = FTimerDelegate::CreateUObject(this, &ABulletHellGameMode::SpawnEnemyShip, EnemySpawn);
+                    GetWorldTimerManager().SetTimer(SpawnDelayTimerHandle, SpawnDelayTimerDelegate, Delay += EnemySpawn->SpawnCountDelay, false);
+                    //Spawn EnemyShip until EnemyCount reached
+            }
+            //exit loop when finished spawning for sequence
+            //increment SpawnSequenceCounter
+            SpawnSequenceCounter++;
+            //Delay before next sequence (next iteration of while loop)
+            FTimerHandle SequenceDelayTimerHandle;
+            //UE_LOG(LogTemp, Display, TEXT("Delay: %f"), Delay + EnemySpawn->DelayBeforeNextSequence);
+            GetWorldTimerManager().SetTimer(SequenceDelayTimerHandle, this, &ABulletHellGameMode::SpawnEnemies, (Delay += EnemySpawn->DelayBeforeNextSequence) / 2, false);
+        }
+        else
+        {
+            UE_LOG(LogTemp, Error, TEXT("Finished Spawning"));
+        }
+        
+    }
+}
+
+void ABulletHellGameMode::SpawnEnemyShip(FEnemySpawns* EnemySpawn) 
+{
+    for(int i = 0; i < EnemySpawn->EnemyCount; i++)
+    {
+        FActorSpawnParameters SpawnParameters;
+        SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+        auto SpawnedEnemyShip = GetWorld()->SpawnActor<AEnemyShip>(EnemyShipClasses[EnemySpawn->EnemyType[i]], EnemySpawn->SpawnLocation[i], EnemySpawn->SpawnRotation[i], SpawnParameters);
+        if(SpawnedEnemyShip)
+        {
+            SpawnedEnemyShip->SetVelocity(EnemySpawn->XYVelocity[i].X, EnemySpawn->XYVelocity[i].Y);
+            SpawnedEnemyShip->SetDestination(EnemySpawn->Destination[i]);
+            SpawnedEnemyShip->SetProjectileRotationOffset(EnemySpawn->ProjectileRotation[i]);
+            UE_LOG(LogTemp, Display, TEXT("Spawned: %s"), *SpawnedEnemyShip->GetActorNameOrLabel());
+        }
+        
+    }
     
+}
+
+int ABulletHellGameMode::GetAliveEnemyShipsCount()
+{
+    TArray<AActor*> EnemyShips;
+    UGameplayStatics::GetAllActorsOfClass(this, AEnemyShip::StaticClass(), EnemyShips);
+    return EnemyShips.Num();
 }
--- Source/BulletHell/BulletHellGameMode.h
+++ Source/BulletHell/BulletHellGameMode.h
@@ -4,11 +4,56 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Engine/DataTable.h"
 #include "BulletHellGameMode.generated.h"
+
 
 /**
  * 
  */
+
+USTRUCT(BlueprintType)
+struct FEnemySpawns : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int Sequence;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float DelayBeforeNextSequence;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int EnemyCount;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<int> EnemyType;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FVector> SpawnLocation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FVector2D> XYVelocity;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float EnemySpawnDelay;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FRotator> ProjectileRotation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FRotator> SpawnRotation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int SpawnCount;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float SpawnCountDelay;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FVector> Destination;
+};
+
 UCLASS()
 class BULLETHELL_API ABulletHellGameMode : public AGameModeBase
 {
@@ -22,6 +67,9 @@ protected:
 
 	UFUNCTION(BlueprintImplementableEvent)
 	void StartGame();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void GameOver(bool bWonGame);
 	
 private:
 	class APlayerShip* PlayerShip;
@@ -34,4 +82,18 @@ private:
 
 	UFUNCTION(BlueprintCallable)
 	void SpawnEnemies();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn")
+	UDataTable* EnemySpawnsDataTable;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn")
+	TArray<TSubclassOf<class AEnemyShip>> EnemyShipClasses;
+
+	void SpawnEnemyShip(FEnemySpawns* EnemySpawn);
+
+	int GetAliveEnemyShipsCount();
+
+	int SpawnSequenceCounter = 0;
+
+	float Delay = 0;
 };
